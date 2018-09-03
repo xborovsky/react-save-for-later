30e190d3a0e1e14da4bb1e0864052f0232aa249d
Marek Borovsky <borovsky@oksystem.cz>
2018-09-03 07:53:48 +0200

    Create backend spring boot module, relocate to common folder for both projects

---
+++ SaveForLater/src/main/java/cz/marek_b/save_for_later_backend/SaveForLaterBackendApplication.java
@@ -0,0 +1,12 @@
+package cz.marek_b.save_for_later_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveForLaterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveForLaterBackendApplication.class, args);
+	}
+}
