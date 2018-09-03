be591227171ba5e008c88cfd4e4c75ade40a2f21
Marek Borovsky <borovsky@oksystem.cz>
2018-09-03 12:48:54 +0200

    Use react router

--- react-save-for-later/src/App.js
+++ react-save-for-later/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Categories from './pages/category-management/Categories';
 
@@ -6,7 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Categories />
+        <Switch>
+          <Route exact path='/' component={Categories}/>
+        </Switch>
       </div>
     );
   }
